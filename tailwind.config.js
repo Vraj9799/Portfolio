d0ff1a27cc6a97c36b5c4880791a72b21a4c845b
Vraj Shah <vrajshah363@gmail.com>
2023-05-01 10:33:50 +0530

    Initial Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,29 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
       },
+      fontFamily: {
+        bodyFont: ["Poppins", "sans-serif"],
+        titleFont: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        bodyColor: "#212428",
+        lightText: "#c4cfde",
+        boxBg: "linear-gradient(145deg, #1e2024, #23272b)",
+        designColor: "#ff014f",
+      },
+      boxShadow: {
+        shadowOne: "10px 10px 19px #1c1e22, -10px -10px 19px #262a2e",
+      }
     },
   },
   plugins: [],
